10084107170b76864e97aa5a234bdeac6e2a6424
mcaniot <mcaniot@aldebaran.com>
2020-01-07 17:00:50 +0100

    test all functions in sphere class

--- tests/robot_sphere_class_test.py
+++ tests/robot_sphere_class_test.py
@@ -3,6 +3,7 @@
 from modular_sphere import SimulationManager
 import os
 import pybullet as p
+import time
 import unittest
 
 class RobotSphereClassTest(unittest.TestCase):
@@ -54,8 +55,8 @@ class RobotSphereClassTest(unittest.TestCase):
         texture_path_name = "This is not a texture"
         try:
             RobotSphereClassTest.robot.setTexture(texture_path_name)
-        except Exception as e:
-            pass
+        except Exception as error:
+            print("Normal error", error)
         self.assertTrue(
             RobotSphereClassTest.robot.getTextureId() is None)
         self.assertTrue(
@@ -63,14 +64,46 @@ class RobotSphereClassTest(unittest.TestCase):
             texture_path_name)
         texture_path_name = "data/texture.png"
         RobotSphereClassTest.robot.setTexture(texture_path_name)
+        time.sleep(1)
         shape_data =\
             p.getVisualShapeData(RobotSphereClassTest.robot.getRobotModel(),
                                  flags=p.VISUAL_SHAPE_DATA_TEXTURE_UNIQUE_IDS,
                                  physicsClientId=RobotSphereClassTest.client)
+        # Texture id is not updating in pybullet code, to check
+        # self.assertTrue(shape_data[0][-1] != -1)
         self.assertTrue(
             RobotSphereClassTest.robot.getTexturePathName() ==\
             texture_path_name)
 
+    def test_move(self):
+        robot_pose_init = RobotSphereClassTest.robot.getPosition()
+        RobotSphereClassTest.robot.move([1, 0, 0])
+        time.sleep(0.5)
+        robot_pose_actual = RobotSphereClassTest.robot.getPosition()
+        self.assertTrue(robot_pose_actual[0] != robot_pose_init[0])
+        self.assertTrue(robot_pose_actual[1] == robot_pose_init[1])
+        self.assertTrue(robot_pose_actual[2] == robot_pose_init[2])
+
+    def test_deformation(self):
+        # not implemented yet, for now the functions are empty
+        RobotSphereClassTest.robot.setDeformation()
+        RobotSphereClassTest.robot.getDeformation()
+
+    def test_scale_dimension(self):
+        robot_pose_init = RobotSphereClassTest.robot.getPosition()
+        init_dimension = RobotSphereClassTest.robot.getDimension()
+        RobotSphereClassTest.robot.setScale(2)
+        self.assertTrue(RobotSphereClassTest.robot.getScale() == 2)
+        robot_pose_actual = RobotSphereClassTest.robot.getPosition()
+        self.assertTrue(robot_pose_actual == robot_pose_init)
+        shape_data =\
+            p.getVisualShapeData(RobotSphereClassTest.robot.getRobotModel(),
+                                 physicsClientId=RobotSphereClassTest.client)
+        actual_dimensions = shape_data[0][3]
+        self.assertTrue(
+            init_dimension*RobotSphereClassTest.robot.getScale() ==\
+            actual_dimensions[0])
+        
 
 if __name__ == "__main__":
     unittest.main()
